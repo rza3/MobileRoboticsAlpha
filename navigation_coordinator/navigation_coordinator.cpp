8de657997ceca3d3f9bd46433cbb1fd41f83714f
wjb48 <98062929+wjb48@users.noreply.github.com>
2022-02-28 18:21:14 -0500

    Added lidar alarm functionality
    Now reacts to lidar alarm

---
+++ navigation_coordinator/navigation_coordinator.cpp
@@ -0,0 +1,91 @@
+#include <ros/ros.h>
+#include "des_state_publisher_service/NavSrv.h"
+#include <std_msgs/Bool.h>
+#include <math.h>
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "navigation_coordinator");
+    ros::NodeHandle n;
+    ros::ServiceClient client = n.serviceClient<des_state_publisher_service::NavSrv>("des_pose_service");
+    
+    des_state_publisher_service::NavSrv pose_srv;
+
+    //Values for all the goal positions
+    //These have to be manually modified for now
+    int numGoals = 9;
+    double x[] = {0.0, 0.5, 0.5, 0.0, 0.5, 0.5, -5, 0.0, 0.0};
+    double y[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 5} ;
+    double psi[] = {0.0, 0.0, 0.0, 0.0, 0.0, 3.1, 3.1, 1.57, 1.57};
+
+    //Mode can be one of four states: 0 = "forward travel", 1 = "spin in place", 2 = "backup", or 3 = "halt"
+    int mode[] = {0, 0, 0, 2, 0, 1, 0, 1, 0};
+
+    double sample_dt = 0.02;
+
+    //Is this necessary this time?
+    //ros::Rate loop_timer(1/sample_dt);
+
+    //Wait until the service is advertized and available before attempting to send goal positions.
+    client.waitForExistence();
+    
+    pose_srv.request.x = x[0];
+    pose_srv.request.y = y[0];
+    pose_srv.request.psi = psi[0];
+    pose_srv.request.mode = mode[0];
+    
+    for(int i = 1; i < numGoals;){
+        ROS_INFO("Reached inside for loop");
+        ROS_INFO("i is %i",i);
+        if (client.call(pose_srv) && ros::ok()){
+            if(pose_srv.response.alarm){
+                ROS_INFO("LIDAR Alarm detected!");
+                //What kind of responses to this do we want?
+            }
+            if (pose_srv.response.alarm && pose_srv.response.failed){
+                ROS_INFO("Waiting for obstacle to move...");
+                //sleep for 5 seconds
+                ros::Duration(5.0).sleep();
+                pose_srv.request.x = 0.0;
+                pose_srv.request.y = 0.0;
+                pose_srv.request.psi = 0.0;
+                pose_srv.request.mode = 3;
+                //check if alarm or failed variables have changed
+                if(client.call(pose_srv)){
+                    if(pose_srv.response.alarm){
+                        ROS_INFO("Obstacle did not move. Backing up...");
+                        pose_srv.request.x = -1 * x[i];
+                        pose_srv.request.y = -1 * y[i];
+                        pose_srv.request.psi = -1 * psi[i];
+                        pose_srv.request.mode = 2;
+                        //code for straight line backup motion request
+                    } else {
+                        ROS_INFO("Obstacle moved. Trying to reconverge on goal pose...");
+                        //do not need to increment i or rewrite request, will use the same goal pose as last time
+                    }
+                } else {
+                    ROS_ERROR("Failed to call service des_pose_service");
+                    return 1;
+                }
+            } else if(!pose_srv.response.alarm && pose_srv.response.failed){
+                ROS_INFO("Failed to converge on goal pose. Stopping Mobot...");
+                pose_srv.request.x = 0.0;
+                pose_srv.request.y = 0.0;
+                pose_srv.request.psi = 0.0;
+                pose_srv.request.mode = 3;
+                //Should we stop the Mobot if it fails to converge on the goal pose? What other actions could we take?
+            } else if(!pose_srv.response.alarm && !pose_srv.response.failed){
+                //Move to next goal in list.
+                ROS_INFO("Successfully converged on goal pose. Sending next goal...");
+                i++;
+                pose_srv.request.x = x[i];
+                pose_srv.request.y = y[i];
+                pose_srv.request.psi = psi[i];
+                pose_srv.request.mode = mode[i];
+            }
+        } else {
+            ROS_ERROR("Failed to call service des_pose_service");
+            return 1;
+        }
+    }
+    return 0;
+}
